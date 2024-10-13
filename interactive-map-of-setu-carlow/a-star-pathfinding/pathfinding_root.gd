da67900684cfa7081cba96e3a7b155bfb958331e
Gábor Major <61381495+NotYetTerminal@users.noreply.github.com>
2024-10-13 20:56:59 +0100

    Create graphical pathfinding

--- interactive-map-of-setu-carlow/a-star-pathfinding/pathfinding_root.gd
+++ interactive-map-of-setu-carlow/a-star-pathfinding/pathfinding_root.gd
@@ -1,6 +1,6 @@
 # Script for implementing A* pathfinding
 # Author: Gábor Major
-# Date created: 2024-10-06
+# Date modified: 2024-10-13
 extends Node3D
 
 @export var starting_waypoint: Waypoint
@@ -10,6 +10,7 @@ func a_star_pathfinding() -> bool:
 	var remaining_waypoints_list: Array[Waypoint] = [starting_waypoint]
 	var checked_waypoints_list: Array[Waypoint] = []
 	
+	end_waypoint.set_colour(Color.BLUE)
 	var current: Waypoint
 	while len(remaining_waypoints_list) != 0:
 		current = remaining_waypoints_list[0]
@@ -22,16 +23,21 @@ func a_star_pathfinding() -> bool:
 		
 		print("\nChecking " + current.name)
 		if current == end_waypoint:
+			await current.found()
 			return true
 		
 		remaining_waypoints_list.erase(current)
 		checked_waypoints_list.append(current)
+		current.set_colour(Color.RED)
+		await get_tree().create_timer(1).timeout
 		
 		for neighbour: Waypoint in current.neighour_waypoints_list:
 			if neighbour in checked_waypoints_list:
 				continue
 			
 			print("Neighour: " + neighbour.name)
+			neighbour.set_colour(Color.ORANGE)
+			await get_tree().create_timer(1).timeout
 			
 			var new_waypoint: bool = neighbour not in remaining_waypoints_list
 			var new_distance_to_neighbour: float = (
@@ -46,12 +52,20 @@ func a_star_pathfinding() -> bool:
 				if new_waypoint:
 					neighbour.h_cost = neighbour.own_coordinate.distance_to(end_waypoint.own_coordinate)
 					remaining_waypoints_list.append(neighbour)
+					neighbour.set_colour(Color.YELLOW)
+					await get_tree().create_timer(1).timeout
 				
 				print("F Cost: " + str(neighbour.f_cost))
 				print("G Cost: " + str(neighbour.g_cost))
 				print("H Cost: " + str(neighbour.h_cost))
+		
+		if current == starting_waypoint:
+			current.set_colour(Color.MAGENTA)
+		else:
+			current.set_colour(Color.GREEN)
+		await get_tree().create_timer(1).timeout
 	
 	return false
 
 func _ready() -> void:
-	print(a_star_pathfinding())
+	print(await a_star_pathfinding())
--- interactive-map-of-setu-carlow/a-star-pathfinding/waypoint.gd
+++ interactive-map-of-setu-carlow/a-star-pathfinding/waypoint.gd
@@ -1,6 +1,6 @@
 # Script for waypoint for A* pathfinding
 # Author: Gábor Major
-# Date created: 2024-10-06
+# Date modified: 2024-10-13
 extends Node3D
 class_name Waypoint
 
@@ -14,9 +14,31 @@ class_name Waypoint
 @export var h_cost: float
 
 @export var neighour_waypoints_list: Array[Waypoint]
-@export var from_waypoint: Waypoint
+@export var from_waypoint: Waypoint:
+	set(value):
+		from_waypoint = value
+		rotate_arrow(value)
 
 
 func _ready() -> void:
 	own_coordinate = base_coordinate + position
-	
+	set_colour(Color.BLACK)
+
+
+func set_colour(new_colour: Color) -> void:
+	var material: StandardMaterial3D = StandardMaterial3D.new()
+	material.albedo_color = new_colour
+	var mesh_instance: MeshInstance3D = get_child(0)
+	mesh_instance.set_surface_override_material(0, material)
+
+
+func rotate_arrow(towards_node: Waypoint) -> void:
+	var arrow_node: Node3D = get_node("Arrow")
+	arrow_node.look_at(Vector3(towards_node.position.x, arrow_node.position.y,towards_node.position.z))
+	arrow_node.show()
+
+
+func found() -> void:
+	set_colour(Color.BLUE)
+	await get_tree().create_timer(1).timeout
+	await from_waypoint.found()
