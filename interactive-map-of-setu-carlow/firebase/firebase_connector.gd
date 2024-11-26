8cf97bc7e665f95204c4ccf3d7ff6db9484d67ad
Gábor Major <61381495+NotYetTerminal@users.noreply.github.com>
2024-11-26 00:02:23 +0000

    Start Firebase connector

--- interactive-map-of-setu-carlow/firebase/firebase_connector.gd
+++ interactive-map-of-setu-carlow/firebase/firebase_connector.gd
@@ -1,1 +1,30 @@
 extends Node
+
+func _ready() -> void:
+	# Connect signals to methods
+	var _error: int = Firebase.Auth.signup_succeeded.connect(on_signup_succeeded)
+	_error = Firebase.Auth.login_failed.connect(on_login_failed)
+	
+	Firebase.Auth.login_anonymous()
+
+# Run on successful sign in
+func on_signup_succeeded(auth: Dictionary) -> void:
+	print(auth)
+	# Get the Base Map Document to work with
+	var base_map_document: FirestoreDocument = (await Firebase.Firestore.list('Base_Map'))[0]
+	print(base_map_document)
+	var base_map_waypoints_collection: Array = await Firebase.Firestore.list('Base_Map/' + base_map_document.doc_name + "/Waypoints")
+	print(base_map_waypoints_collection)
+	
+	
+
+# Login unavailable, either authentication messed up or no internet.
+func on_login_failed(error_code: String, message: String) -> void:
+	print("error code: " + error_code)
+	if error_code == "Connection error":
+		# Handle no internet connection
+		print("message: " + message)
+	else:
+		# Handle authentication error
+		print("message: " + message)
+	
