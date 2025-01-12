dacc852b20988d5d8379be5e97836adde87aad63
Gábor Major <61381495+NotYetTerminal@users.noreply.github.com>
2025-01-12 13:01:09 +0000

    Add Equirectangular projection

--- interactive-map-of-setu-carlow/structures/structure.gd
+++ interactive-map-of-setu-carlow/structures/structure.gd
@@ -20,15 +20,15 @@ func update_waypoints_time(new_time: int) -> void:
 
 # Set the position of the structue based on the GPS coordinates
 func set_structure_global_position() -> void:
-	var this_position: Vector3 = convert_longitude_latitude_to_coordinate(longitude, latitude)
+	var this_position: Vector3 = equirectangular_conversion(longitude, latitude)
 	@warning_ignore("unsafe_property_access")
 	@warning_ignore("unsafe_call_argument")
-	var global_base_position: Vector3 = convert_longitude_latitude_to_coordinate(Globals.base_longitude, Globals.base_latitude)
+	var global_base_position: Vector3 = equirectangular_conversion(Globals.base_longitude, Globals.base_latitude)
 	
-	global_position = (this_position - global_base_position) / 20
+	global_position = (this_position - global_base_position) / 10
 
 # Convert using Mercator projection
-func convert_longitude_latitude_to_coordinate(lon: float, lat: float) -> Vector3:
+func mercator_conversion(lon: float, lat: float) -> Vector3:
 	# Convert latitude and longitude to radians
 	var longitude_radians: float = deg_to_rad(lon)
 	var latitude_radians: float = deg_to_rad(lat)
@@ -36,6 +36,19 @@ func convert_longitude_latitude_to_coordinate(lon: float, lat: float) -> Vector3
 	# Calculate coordinates using the Mercator projection
 	var x_coordinate: float = longitude_radians
 	var z_coordinate: float = log(tan(PI / 4.0 + latitude_radians / 2.0))
+
+	@warning_ignore("unsafe_property_access")
+	return Vector3(x_coordinate, 0, z_coordinate) * Globals.EARTH_RADIUS
+
+# Convert using Equirectangular projection
+func equirectangular_conversion(lon: float, lat: float) -> Vector3:
+	# Convert latitude and longitude to radians
+	var longitude_radians: float = deg_to_rad(lon)
+	var latitude_radians: float = deg_to_rad(lat)
+	
+	# Calculate coordinates using the Equirectangular projection
+	var x_coordinate: float = longitude_radians * cos(deg_to_rad(Globals.base_latitude))
+	var z_coordinate: float = latitude_radians
 
 	@warning_ignore("unsafe_property_access")
 	return Vector3(x_coordinate, 0, z_coordinate) * Globals.EARTH_RADIUS
