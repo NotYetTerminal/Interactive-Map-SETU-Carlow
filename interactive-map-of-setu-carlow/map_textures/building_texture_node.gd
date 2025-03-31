c3f355c2a859c08a295c4212824b0cda86a9cfe1
Gábor Major <61381495+NotYetTerminal@users.noreply.github.com>
2025-03-31 16:29:33 +0100

    Bur out buildings

--- interactive-map-of-setu-carlow/map_textures/building_texture_node.gd
+++ interactive-map-of-setu-carlow/map_textures/building_texture_node.gd
@@ -4,9 +4,9 @@ class_name BuildingTextureNode
 
 func update_visibility() -> void:
 	var index: int = 1
-	for mesh_instance_3d: MeshInstance3D in get_children():
+	for node_3d: Node3D in get_children():
 		if index == Globals.current_floor:
-			mesh_instance_3d.visible = true
+			node_3d.visible = true
 		else:
-			mesh_instance_3d.visible = false
+			node_3d.visible = false
 		index += 1
